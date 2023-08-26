5a4ef6c32b10b705f2da175a80f7d483625ffadd
Hanul Rheem <rheemhanul@gmail.com>
2023-08-27 00:23:48 +1200

    [Added] background animations are established. inout effect implemented

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Home from "../src/pages/home/home.tsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
 )
