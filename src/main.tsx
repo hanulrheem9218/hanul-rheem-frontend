cd0c3b7761c909b09df3a416fa16bdb42572665a
Hanul Rheem <rheemhanul@gmail.com>
2023-09-06 17:56:52 +1200

    [Fixed] removed restirceted mode in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,16 +9,14 @@ import Admin from './pages/admin/admin.tsx'
 import Project from './pages/projects/projects.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="/home" element={<App />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Project />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="/home" element={<App />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Project />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
 )
